62e783d0561b0a08582b3f624aaf2f1059e688fb
juju4 <juju4@users.noreply.github.com>
2017-08-27 10:38:20 -0400

    serverspec tests: refine tests with stderr and exit status

--- test/integration/default/serverspec/rekall_spec.rb
+++ test/integration/default/serverspec/rekall_spec.rb
@@ -14,15 +14,25 @@ describe command('/usr/local/share/env-rekall/bin/pip freeze') do
   its(:stdout) { should match /rekall-yara/ }
   its(:stdout) { should match /six/ }
 end
+
 #describe file('/usr/local/bin/rekall') do
 describe file('/usr/local/share/env-rekall/bin/rekall') do
   it { should exist }
   it { should be_executable }
 end
+
 describe command('/usr/local/share/env-rekall/bin/rekall --version') do
   its(:stdout) { should match /This is Rekall Version/ }
-end
-describe command('/usr/local/bin/rekall --version') do
-  its(:stdout) { should match /This is Rekall Version/ }
+  its(:stderr) { should_not match /pkg_resources.ContextualVersionConflict/ }
+  its(:exit_status) { should eq 0 }
 end
 
+describe file('/usr/local/bin/rekall') do
+  it { should be_symlink }
+end
+
+describe command('/usr/local/bin/rekall --version') do
+  its(:stdout) { should match /This is Rekall Version/ }
+  its(:stderr) { should_not match /pkg_resources.ContextualVersionConflict/ }
+  its(:exit_status) { should eq 0 }
+end
